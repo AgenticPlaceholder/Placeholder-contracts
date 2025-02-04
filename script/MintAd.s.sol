01c5c73c116f4647b81b0277753f56abf8db8333
George Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2025-02-04 09:32:22 +0530

    satisfying forge

--- script/MintAd.s.sol
+++ script/MintAd.s.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.19;
 
 import { PlaceholderAdsNFT } from "../src/PlaceholderAdsNFT.sol";
-import { console2 } from "forge-std/Console2.sol";
 import { Script } from "forge-std/Script.sol";
+import { console2 } from "forge-std/console2.sol";
 
 contract MintAdScript is Script {
     function run() external {
